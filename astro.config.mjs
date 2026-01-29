a776058caf22599e50b04f2a0986feb7b5d88e1c
tubachap <138092035+tubachap@users.noreply.github.com>
2026-01-29 09:00:11 -0500

    Add SOC-ML project page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://brianchaplow.com',
-  integrations: [
-    mdx(),
-    tailwind(),
-    sitemap({
-      filter: (page) => !page.includes('/resume-thanks/'),
-    }),
-  ],
+  integrations: [mdx(), tailwind(), sitemap({
+    filter: (page) => !page.includes('/resume-thanks/'),
+  }), react()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
